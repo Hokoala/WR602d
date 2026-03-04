b7a7e8430c23cc199c4b1206d10593a990c3c9a0
Hokoala <jean-michel.le@etudiant.univ-reims.fr>
2026-03-04 10:05:06 +0100

    feat: secure routes and implement plan-based tool access with Voter

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -122,6 +122,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
+        // Ajoute le rôle du plan (ex: ROLE_PREMIUM, ROLE_PRO…)
+        if ($this->plan && $this->plan->getRole()) {
+            $roles[] = $this->plan->getRole();
+        }
+
         return array_unique($roles);
     }
 
